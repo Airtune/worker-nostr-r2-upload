cc80bc0d0630903fa641e27f8b7a1e9ae5b6859d
Airtune <84182637+Airtune@users.noreply.github.com>
2023-08-19 15:51:17 -0400

    Use File Metadata Event as Authorization
    * rename metadata to fileMetadata to avoid confusion between kind 0 and kind 1063 events
    * replace authorization header with base64 kind 1063 event
    * rename user.id to user.pubkey for consistent naming
    * update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,6 +1,7 @@
 export type { R2Bucket, R2Object, R2ObjectBody, R2GetOptions, KVNamespace, ModuleWorkerContext } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.12/common/cloudflare_workers_types.d.ts';
 
-export { type Event, nip26, nip98, Kind as EventKind, getBlankEvent, finishEvent } from "npm:airtune-nostr-tools-development@1.14.2";
+export { type Event, nip26, nip98, getBlankEvent, finishEvent, validateEvent, verifySignature } from "npm:airtune-nostr-tools-development@1.14.3";
+export { type EventTemplate } from 'npm:nostr-tools';
 export { bytesToHex} from "npm:@noble/hashes@1.3.1/utils"; // use same dependency as nostr-tools
 
 export { router, type HandlerContext, type MatchHandler } from "https://deno.land/x/rutt@0.1.0/mod.ts";
--- index.ts
+++ index.ts
@@ -1,35 +1,47 @@
-import { EventTemplate } from 'npm:nostr-tools';
-import { R2Object, Event, EventKind, nip98, nip26, bytesToHex, getBlankEvent, finishEvent, router, HandlerContext, MatchHandler, ModuleWorkerContext } from './deps.ts'
+import {
+  type Event,
+  type EventTemplate,
+  type HandlerContext,
+  type MatchHandler,
+  type ModuleWorkerContext,
+  type R2Object,
+  bytesToHex,
+  finishEvent,
+  getBlankEvent,
+  nip26,
+  nip98,
+  router,
+  validateEvent,
+  verifySignature
+} from './deps.ts'
 import type { WorkerEnv } from "./worker_env.d.ts";
-
 
 const hexKeyPattern = /^[0-9A-Fa-f]{64}$/;
 
 type Role = "admin" | "user" | "banned";
 
-/** NIP-98 authentication event */
-type AuthEvent = Event & { kind: EventKind.HttpAuth }
-/** NIP-94 file-metadata event */
-type MetadataEvent = Event & { kind: 1063 }
+/** NIP-94 File Metadata Event */
+type FileMetadataEvent = Event & { kind: 1063 };
 
 /**
- * Verify a request NIP-98 authorization.
+ * File Metadata Event (kind 1063) is base64 encoded as the authorization header.
  * 
- * @returns Either a validated `kind 27235` event, or a object containing an error message.
+ * @returns Either a validated `kind 1063` event, or a object containing an error message.
  */
-async function get_auth_event(request: Request): Promise<AuthEvent | { error: string }> {
+async function get_file_metadata_event(request: Request): Promise<FileMetadataEvent | { error: string }> {
   try {
     const token = request.headers.get('Authorization')
     if (!token) return { error: 'missing Authorization header' }
-    const event: AuthEvent = (await nip98.unpackEventFromToken(token)) as AuthEvent
-    if (await nip98.validateEvent(event, request.url, request.method)) {
-      return event
+    const event: Event = (await nip98.unpackEventFromToken(token)) as Event;
+
+    if (validateEvent(event) && event.kind === 1063 && verifySignature(event)) {
+      return event as FileMetadataEvent;
     } else {
       return { error: 'Invalid nostr event.'}
     }
     
   } catch (error) {
-    console.debug('Failed NIP-98 authentication.', error)
+    console.debug('Authentication failed for File Metadata Event (kind 1063)', error)
     if (error instanceof Error) {
       return { error: error.message }
     } else {
@@ -52,13 +64,13 @@ type Context = {
  * Context for authenticated route handlers. 
  */
 type AuthContext = Context & {
-  /** Verified `kind 27235` event from the request Authorization header. */
-  authEvent: AuthEvent
+  /** Verified `kind 1063` event from the request Authorization header. */
+  fileMetadataEvent: FileMetadataEvent
   
-  /** Detaisl about the authenticated user. */
+  /** Details about the authenticated user. */
   user: {
-    /** The user id, same as `authEvent.pubkey` */
-    id: string
+    /** The user pubkey, same as `fileMetadataEvent.pubkey` */
+    pubkey: string
     /** The user's role (as retrieved with `BANBOORU_PUBKEY_ROLE_KV.get(userId)` */
     role: Role | undefined
   }
@@ -69,15 +81,15 @@ type AuthContext = Context & {
  */
 function auth(next: MatchHandler<AuthContext>): MatchHandler<Context> {
   return async (request: Request, ctx: HandlerContext<Context>, params: Record<string, string>) => {
-    const authEvent: AuthEvent | { error: string } = await get_auth_event(request);
-    if ("error" in authEvent) {
-      return new Response(`Unauthorized: ${authEvent.error}`, { status: 401 });
+    const fileMetadataEvent: FileMetadataEvent | { error: string } = await get_file_metadata_event(request);
+    if ("error" in fileMetadataEvent) {
+      return new Response(`Unauthorized: ${fileMetadataEvent.error}`, { status: 401 });
     } else {
       return next(request, Object.assign(ctx, {
-        authEvent,
+        fileMetadataEvent,
         user: {
-          id: authEvent.pubkey,
-          role: await ctx.env.BANBOORU_PUBKEY_ROLE_KV.get(authEvent.pubkey) as Role | undefined
+          pubkey: fileMetadataEvent.pubkey,
+          role: await ctx.env.BANBOORU_PUBKEY_ROLE_KV.get(fileMetadataEvent.pubkey) as Role | undefined
         }
       }), params)
     }
@@ -91,23 +103,20 @@ function auth(next: MatchHandler<AuthContext>): MatchHandler<Context> {
  * @param object The object itself.
  * @param env The worker environment.
  */
-async function handleMetadada(baseUrl: string, object: R2Object, env: WorkerEnv, delegation?: nip26.Delegation): Promise<void> {
+async function handleMetadada(baseUrl: string, object: R2Object, env: WorkerEnv): Promise<void> {
   if(!object.checksums.sha256) {
     throw new Error("Failed to build file metadata: missing sha256 checksum.")
   }
-  const metadataEvent: EventTemplate<1063> = getBlankEvent(1063) // 1063 is NIP-94 event kind for file metadata
-  metadataEvent.tags.push(['url', `${baseUrl}/${object.key}`])
-  metadataEvent.tags.push(['m', object.httpMetadata.contentType || 'application/octet-stream'])
-  metadataEvent.tags.push(['x',  bytesToHex(new Uint8Array(object.checksums.sha256))])
-  metadataEvent.tags.push(['size', object.size.toString()])
+  const fileMetadataEvent: EventTemplate<1063> = getBlankEvent(1063) // 1063 is NIP-94 event kind for file metadata
+  fileMetadataEvent.tags.push(['url', `${baseUrl}/${object.key}`])
+  fileMetadataEvent.tags.push(['m', object.httpMetadata.contentType || 'application/octet-stream'])
+  fileMetadataEvent.tags.push(['x',  bytesToHex(new Uint8Array(object.checksums.sha256))])
+  fileMetadataEvent.tags.push(['size', object.size.toString()])
   // TODO dim (image dimensions) requires reading image header bytes
   // TODO i (torrent infohash) and magnet (Magnet URI)- requires generating a .torrent file (which could be stored in the bucket too)
   // TODO blurhash - requires rendering the image
-  if (delegation) {
-    metadataEvent.tags.push(['delegation', delegation.from, delegation.cond, delegation.sig])
-  }
-  const signedMetadata = finishEvent(metadataEvent, env.PRIVATE_KEY)
-  await env.BANBOORU_BUCKET.put(`${object.key}.metadata.json`, JSON.stringify(signedMetadata))
+  const signedEvent = finishEvent(fileMetadataEvent, env.PRIVATE_KEY)
+  await env.BANBOORU_BUCKET.put(`${object.key}.metadata.json`, JSON.stringify(signedEvent))
 
   // TODO Send `kind 1063` event to nostr relay(s)
 
@@ -115,7 +124,7 @@ async function handleMetadada(baseUrl: string, object: R2Object, env: WorkerEnv,
 }
 
 /**
- * Restricted access middleware. Requires a NIP-94 Authorization header from a known public key with one of the specific `roles`.
+ * Restricted access middleware. Requires a known public key with one of the specific `roles` from a base64 encoded File Metadata Event in the Authorization header.
  */
 function restricted(roles: Role[], next: MatchHandler<AuthContext>): MatchHandler<Context> {
   return auth((request: Request, ctx: HandlerContext<AuthContext>, params: Record<string, string>) => {
@@ -130,16 +139,16 @@ function restricted(roles: Role[], next: MatchHandler<AuthContext>): MatchHandle
 /**
  * Returns whether a specific user is the publisher of a nostr event.
  */
-function isPublisher(userId: string, event: Event): boolean {
-  return event.pubkey === userId;
+function isPublisher(pubkey: string, event: Event): boolean {
+  return event.pubkey === pubkey;
 }
 
 /**
  * Returns whether a specific user delegated signature of a nostr event.
  */
-function isDelegator(userId: string, event: MetadataEvent): boolean {
+function isDelegator(pubkey: string, event: Event): boolean {
   try {
-    return nip26.getDelegator(event) === userId;
+    return nip26.getDelegator(event) === pubkey;
   } catch (error) {
     console.log('Failed to get NIP-26 delegator from event', event, error)
     return false
@@ -169,18 +178,15 @@ const handler = router<Context>({
     headers.set("etag", object.httpEtag);
     return new Response(object.body, { headers });
   },
-  'PUT@/file/:hash': restricted(["admin", "user"], async function putFile(request, { env, authEvent, workerContext, user }, params) {
+  'PUT@/file/:hash': restricted(["admin", "user"], async function putFile(request, { env, fileMetadataEvent, workerContext }, params) {
     if(!params.hash.match(hexKeyPattern)) return new Response(`Invalid SHA256 hash: ${params.hash}`, { status: 400 });
     if(!request.body) return new Response("Missing body", { status: 400 });
 
-    // We expect the SHA256 hash from the URL to match the one of the request body (ie. the uploaded file).
-    // Per NIP-98 the authentication event SHOULD include a SHA256 hash of the request body in a 'payload' tag as hex (["payload", "<sha256-hex>"]).
-    // We will validate that the actual payload hash match when uploading to R2.
-    const payloadTag = authEvent.tags.find((t: string[]) => t[0] === 'payload')
-    if(payloadTag && payloadTag[1] && payloadTag[1] != params.hash) {
-      return new Response(`Unauthorized: Invalid nostr event, payload signature does not match URL`, { status: 401 });
+    // We expect the SHA256 hash from the URL to match the one of the event in the Authorization header.
+    const sha256Tag = fileMetadataEvent.tags.find((t: string[]) => t[0] === 'x');
+    if (sha256Tag && typeof(sha256Tag[1]) === 'string' && sha256Tag[1].toUpperCase() !== params.hash.toUpperCase()) {
+      return new Response(`Mismatch between event SHA256 hash (${sha256Tag[1]}) and url path SHA256 hash (${params.hash})`, { status: 400 });
     }
-
     try {
       if(await env.BANBOORU_BUCKET.head(params.hash)) {
         return new Response("Conflict", { status: 409 })
@@ -188,33 +194,17 @@ const handler = router<Context>({
       const object = await env.BANBOORU_BUCKET.put(params.hash, request.body, {
         // See https://developers.cloudflare.com/r2/api/workers/workers-api-reference/#r2putoptions
         // R2 will check the received data SHA-256 hash to confirm received object integrity and will fail if it does not.
-        sha256: params.hash,
+        sha256: params.hash, // We expect the SHA256 hash from the URL to match the one of the request body (ie. the uploaded file).
         httpMetadata: {
           contentType: request.headers.get('content-type') || undefined,
           cacheControl: 'public, max-age=31536000, immutable' // let's client cache this for a year
         }
       });
       if (!object) {
-        if(payloadTag && payloadTag[1]) { // The user signed the hash as part of the auth event. 
-          return new Response(`Unauthorized: Invalid nostr event, payload signature invalid`, { status: 401 });
+        if(sha256Tag && sha256Tag[1]) { // The user signed the hash as part of the event. 
+          return new Response(`Unauthorized: Invalid nostr event, SHA256 hash does not match file`, { status: 401 });
         } else {
           return new Response("Bad Request", { status: 400 })
-        }
-      }
-
-      let nip26Delegation: nip26.Delegation | undefined;
-      if (request.headers.has("x-nip-26-delegation")) {
-        const nip26DelegationHeader: string | null = request.headers.get("x-nip-26-delegation");
-        if (nip26DelegationHeader) {
-          const parsedHeader = JSON.parse(nip26DelegationHeader);
-          const isNip26Delegation = typeof(parsedHeader) === 'object' &&
-                                    typeof(parsedHeader.from) === 'string' &&
-                                    typeof(parsedHeader.cond) === 'string' &&
-                                    typeof(parsedHeader.sig) === 'string';
-          
-          if (isNip26Delegation) {
-            nip26Delegation = parsedHeader;
-          }
         }
       }
 
@@ -222,9 +212,7 @@ const handler = router<Context>({
       workerContext.waitUntil(handleMetadada(
         `https://${request.headers.get('host')}`,
           object,
-          env,
-          // TODO Safer parsing of the NIP Delegation
-          nip26Delegation
+          env
         ))
 
       return new Response(null, { status: 204 }) // Success response with no content.
@@ -238,18 +226,18 @@ const handler = router<Context>({
 
     try {
       // Check that the file exists.
-      const object = await env.BANBOORU_BUCKET.get(params.hahs)
+      const object = await env.BANBOORU_BUCKET.get(params.hash)
       if(!object) return new Response("Not Found", { status: 404 })
 
       // Retrieve the stored `kind 1063` event with the file metadata.
-      const metadataObject = await env.BANBOORU_BUCKET.get(`${params.hahs}.metadata.json`)
-      const metadata: Event | null = metadataObject ? (await metadataObject.json()) as MetadataEvent : null
+      const fileMetadataObject = await env.BANBOORU_BUCKET.get(`${params.hash}.metadata.json`)
+      const fileMetadata: Event | null = fileMetadataObject ? (await fileMetadataObject.json()) as FileMetadataEvent : null
   
       // Either the user is admin, or the user signed the `kind 1063` event (possibly delegating the signature).
-      if (user.role == 'admin' || (metadata && (isPublisher(user.id, metadata) || isDelegator(user.id, metadata)))) {
+      if (user.role == 'admin' || (fileMetadata && (isPublisher(user.pubkey, fileMetadata) || isDelegator(user.pubkey, fileMetadata)))) {
           await Promise.all([
             env.BANBOORU_BUCKET.delete(params.hash),
-            env.BANBOORU_BUCKET.delete(`${params.hahs}.metadata.json`)
+            env.BANBOORU_BUCKET.delete(`${params.hash}.metadata.json`)
           ])
   
           // TODO Send delete nostr event to rely(s)
@@ -270,12 +258,12 @@ export default {
   fetch(request: Request, env: WorkerEnv, workerContext: ModuleWorkerContext) {
     // Faking ConnInfo localAddr and remoteAddr expected by rutt that we won't actually use.
     const localAddr = {
-      transport: 'tcp' as 'tcp',
+      transport: 'tcp' as const,
       hostname: request.headers.get('CF-Worker') || '',
       port: -1,
     }
     const remoteAddr = {
-      transport: 'tcp' as 'tcp',
+      transport: 'tcp' as const,
       hostname: request.headers.get('CF-Connecting-IP') || '',
       port: -1
     }
