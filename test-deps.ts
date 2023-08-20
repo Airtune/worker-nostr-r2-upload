d2f08d1b01ffee2a4a8a8578a9882b8af56227d6
Airtune <84182637+Airtune@users.noreply.github.com>
2023-08-20 12:48:50 -0400

    import from both deps.ts and test-deps.ts in tests

--- test-deps.ts
+++ test-deps.ts
@@ -1,3 +1,3 @@
-export type { R2Bucket, R2Object, R2ObjectBody, R2GetOptions, R2PutOptions, KVNamespace, ModuleWorkerContext } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.12/common/cloudflare_workers_types.d.ts';
+export type { R2Object } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.12/common/cloudflare_workers_types.d.ts';
 
-export { type Event, getBlankEvent, generatePrivateKey, finishEvent } from "npm:airtune-nostr-tools-development@1.14.3";
+export { getBlankEvent, finishEvent } from "npm:airtune-nostr-tools-development@1.14.3";
